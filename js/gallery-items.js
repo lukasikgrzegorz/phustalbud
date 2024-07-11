9defa756babb0c146646b2ba6b185cc049e26032
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2024-07-11 11:19:43 +0200

    Add mobile styles and photos

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,65 +1,42 @@
 export const galleryItems = [
   {
-    preview:
-      './images/photo_1_min.jpg',
-    original:
-      './images/photo_1.jpg',
-    description: 'Konstrukcja',
+    preview: "./images/photo_1_min.jpg",
+    original: "./images/photo_1.jpg",
+    description: "Zdjęcie",
   },
   {
-   preview:
-      './images/photo_2_min.jpg',
-    original:
-      './images/photo_2.jpg',
-    description: 'Hala stalowa',
+    preview: "./images/photo_2_min.jpg",
+    original: "./images/photo_2.jpg",
+    description: "Zdjęcie",
   },
   {
-    preview:
-      './images/photo_3_min.jpg',
-    original:
-      './images/photo_3.jpg',
-    description: 'Hala stalowa',
+    preview: "./images/photo_3_min.jpg",
+    original: "./images/photo_3.jpg",
+    description: "Zdjęcie",
   },
   {
-   preview:
-      './images/photo_4_min.jpg',
-    original:
-      './images/photo_4.jpg',
-    description: 'Hala stalowa',
+    preview: "./images/photo_4_min.jpg",
+    original: "./images/photo_4.jpg",
+    description: "Zdjęcie",
   },
   {
-   preview:
-      './images/photo_5_min.jpg',
-    original:
-      './images/photo_5.jpg',
-    description: 'Hala stalowa',
+    preview: "./images/photo_5_min.jpg",
+    original: "./images/photo_5.jpg",
+    description: "Zdjęcie",
   },
   {
-   preview:
-      './images/photo_6_min.jpg',
-    original:
-      './images/photo_6.jpg',
-    description: 'Hala stalowa',
+    preview: "./images/photo_6_min.jpg",
+    original: "./images/photo_6.jpg",
+    description: "Zdjęcie",
   },
   {
-    preview:
-      './images/photo_7_min.jpg',
-    original:
-      './images/photo_7.jpg',
-    description: 'Hala stalowa',
+    preview: "./images/photo_7_min.jpg",
+    original: "./images/photo_7.jpg",
+    description: "Zdjęcie",
   },
   {
-    preview:
-      './images/photo_8_min.jpg',
-    original:
-      './images/photo_8.jpg',
-    description: 'Konstrukcja',
-  },
-  {
-    preview:
-      './images/photo_9_min.jpg',
-    original:
-      './images/photo_9.jpg',
-    description: 'Garaż',
+    preview: "./images/photo_8_min.jpg",
+    original: "./images/photo_8.jpg",
+    description: "Zdjęcie",
   },
 ];
